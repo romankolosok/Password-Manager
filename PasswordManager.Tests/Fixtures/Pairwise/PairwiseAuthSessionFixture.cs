666e55ca44712d68f6d328e824bc4fb57bd7d280
Roman Kolosok <romakolosok14@gmail.com>
2026-04-12 17:15:33 -0400

    Add session management tests for AuthService

--- PasswordManager.Tests/Fixtures/Pairwise/PairwiseAuthSessionFixture.cs
+++ PasswordManager.Tests/Fixtures/Pairwise/PairwiseAuthSessionFixture.cs
@@ -6,37 +6,45 @@ using PasswordManager.Core.Services.Interfaces;
 
 namespace PasswordManager.Tests.Fixtures.Pairwise
 {
-    public class PairwiseAuthCryptoFixture
+    public class PairwiseAuthSessionFixture
     {
         public Mock<IAuthClient> AuthClient { get; } = new();
+        public Mock<ICryptoService> CryptoService { get; } = new();
         public Mock<IUserProfileService> UserProfileService { get; } = new();
         public Mock<IVaultRepository> VaultRepository { get; } = new();
-        public Mock<ISessionService> SessionService { get; } = new();
         public Mock<IAuthExceptionMapper> ExceptionMapper { get; } = new();
         public Mock<ILogger<AuthService>> Logger { get; } = new();
 
-        private readonly ICryptoService _cryptoService = new CryptoService();
+        private ISessionService _sessionService;
 
-        public ICryptoService Crypto => _cryptoService;
+        public ISessionService SessionService => _sessionService;
+
+        public PairwiseAuthSessionFixture()
+        {
+            _sessionService = new SessionService();
+        }
 
         public AuthService CreateService() =>
             new(
                 AuthClient.Object,
-                _cryptoService,
+                CryptoService.Object,
                 UserProfileService.Object,
                 VaultRepository.Object,
-                SessionService.Object,
+                _sessionService,
                 ExceptionMapper.Object,
                 Logger.Object);
 
         public void Reset()
         {
             AuthClient.Reset();
+            CryptoService.Reset();
             UserProfileService.Reset();
             VaultRepository.Reset();
-            SessionService.Reset();
             ExceptionMapper.Reset();
             Logger.Reset();
+
+            _sessionService.Dispose();
+            _sessionService = new SessionService();
         }
     }
 }

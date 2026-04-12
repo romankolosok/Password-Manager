7753754b78c1206dc9edf6d1b7552d34e9f0dbb7
Roman Kolosok <romakolosok14@gmail.com>
2026-04-12 17:11:08 -0400

    Add AuthService unit tests with fake auth client fixture

--- PasswordManager.Tests/Fixtures/Pairwise/PairwiseAuthFakeAuthFixture.cs
+++ PasswordManager.Tests/Fixtures/Pairwise/PairwiseAuthFakeAuthFixture.cs
@@ -7,34 +7,38 @@ using PasswordManager.Tests.Fakes;
 
 namespace PasswordManager.Tests.Fixtures.Pairwise
 {
-    public class PairwiseAuthRepoFixture
+    public class PairwiseAuthFakeAuthFixture
     {
-        public IVaultRepository Repository { get; private set; } = new InMemoryVaultRepository();
-        public Mock<IAuthClient> AuthClient { get; } = new();
+        private IAuthClient _fakeAuthClient = new FakeAuthClient();
+
+        public IAuthClient FakeAuthClient => _fakeAuthClient;
+
         public Mock<ICryptoService> CryptoService { get; } = new();
         public Mock<IUserProfileService> UserProfileService { get; } = new();
+        public Mock<IVaultRepository> VaultRepository { get; } = new();
         public Mock<ISessionService> SessionService { get; } = new();
         public Mock<IAuthExceptionMapper> ExceptionMapper { get; } = new();
         public Mock<ILogger<AuthService>> Logger { get; } = new();
 
-        public AuthService CreateService() => new(
-            AuthClient.Object,
-            CryptoService.Object,
-            UserProfileService.Object,
-            Repository,
-            SessionService.Object,
-            ExceptionMapper.Object,
-            Logger.Object);
+        public AuthService CreateService() =>
+            new(
+                _fakeAuthClient,
+                CryptoService.Object,
+                UserProfileService.Object,
+                VaultRepository.Object,
+                SessionService.Object,
+                ExceptionMapper.Object,
+                Logger.Object);
 
         public void Reset()
         {
-            Repository = new InMemoryVaultRepository();
-            AuthClient.Reset();
             CryptoService.Reset();
             UserProfileService.Reset();
+            VaultRepository.Reset();
             SessionService.Reset();
             ExceptionMapper.Reset();
             Logger.Reset();
+            _fakeAuthClient = new FakeAuthClient();
         }
     }
 }
